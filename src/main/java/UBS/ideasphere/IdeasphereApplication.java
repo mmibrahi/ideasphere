0d5948ad43b7b71cddcdbd6b69de83cc821a97a8
mmibrahi <111172010+mmibrahi@users.noreply.github.com>
2024-10-28 20:53:28 +0300

    Initial commit

---
+++ src/main/java/UBS/ideasphere/IdeasphereApplication.java
@@ -0,0 +1,13 @@
+package UBS.ideasphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeasphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeasphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/UBS/ideasphere/IdeasphereApplicationTests.java
@@ -0,0 +1,13 @@
+package UBS.ideasphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeasphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
